3ea96c75816d5f2b5add27ab4b11ba82ce605066
lordrahl2-sys <s65848383@gmail.com>
2025-07-12 19:25:24 +0300

    Create owner.js

---
+++ commands/owner.js
@@ -0,0 +1,9 @@
+module.exports = async (client, msg) => {
+  client.sendText(msg.from, `
+╭━━〔 *⎈ Lord Rahl ⎈* 〕━━┈⊷
+┃◈ *Owner:* 254740694462
+┃◈ *Version:* 7.0.1
+┃◈ *Support:* +254740694462
+╰━━━━━━━━━━━━━━━⊷
+`);
+};
