fb4f20c89fdcfb37a1a87a2b5562c4089bc683f0
lordrahl2-sys <s65848383@gmail.com>
2025-07-13 01:14:12 +0300

    Update welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -4,10 +4,16 @@ module.exports = async (client, message) => {
   const { chatId, isGroupMsg, event, contact } = message;
 
   if (event === "add" && isGroupMsg) {
-    const name = contact?.pushname || "new member";
+    const name = contact?.pushname || "noble guest";
 
-    const welcomeText = `👋 Welcome *${name}* to the group!
-I’m *Lord Rahl Venom Bot*. Type *!menu* to get started.`;
+    const welcomeText = `
+╭━━━〔 *🎉 WELCOME TO THE KINGDOM* 〕━━━╮
+┃ 👑 Hail *${name}*!
+┃ ✦ You’ve entered a royal chamber.
+┃ 🤖 I am *Lord Rahl Venom Bot*.
+┃ 📜 Type *.menu* to summon my powers.
+╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
+    `;
 
     client.sendText(chatId, welcomeText).catch(console.error);
   }
